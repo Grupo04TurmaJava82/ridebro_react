72dbc9d2be174ff31ae007e29d2c1adcb9ec5929
Iago Zandone <iagozandone@gmail.com>
2025-07-04 16:37:18 -0300

    :sparkles: Criação do Navbar e Footer

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    // Adicione esta div em cada página
+    <div className="flex-grow flex flex-col justify-center items-center">
+      <h1 className="text-2xl">Página Inicial</h1>
+      <p>Bem-vindo ao RideBro!</p>
+    </div>
+  );
+};
+
+export default Home;
